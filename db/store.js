b6c922c16ed5d9a26b4fae25ff96e837aa2b9c38
depotte6 <depotte6@gmail.com>
2021-11-03 18:36:30 -0400

    added functions to add, deleted update neots

--- db/store.js
+++ db/store.js
@@ -1,15 +1,48 @@
 const fs = require('fs');
 const util = require('util');
-const uuid = require('uuid/v1)');
+const { v4: uuidv4 } = require('uuid');
 
-const writeFileAsync = require('util.promisify(fs.writeFile');
-const readFileAsync = require('util.promisify(fs.readFile');
-const { Store } = require('express-session');
+const writeFileAsync = util.promisify(fs.writeFile);
+const readFileAsync = util.promisify(fs.readFile);
 
 class Store {
     read() {
-        return fs.readFileAsync('db/db.json', 'utf8');
+        return readFileAsync('db/db.json', 'utf8');
     }
-}
+    write(note) {
+        return writeFileAsync('db/db.json', JSON.stringify(note));
+    }
+    getNotes() {
+        return this.read().then((notes) => {
+            let parsedNotes;
+            try {
+                parsedNotes = [].concat(JSON.parse(notes));
+            } catch (err) {
+                parsedNotes = [];
+            }
+            return parsedNotes;
+        });
+    }
+    addNote(note) {
+        const { title, text } = note;
+        if (!title || !text) {
+            throw new Error("note must have title and text");
+        }
+        const newNote = { title, text, id:uuidv4() };
+        return this.getNotes()
+            .then((notes) => [...notes, newNote])
+            .then((updatedNotes) => this.write(updatedNotes))
+            .then(() => newNote);
+         }
 
-module exports = new Store();
+        removeNote(id) {
+            return this.getNotes()
+            .then((notes) => notes.filter((note) => note.id = id))
+            .then((filteredNotes) => this.write(filteredNotes));
+        }
+      }
+       
+
+
+
+module.exports = new Store();
